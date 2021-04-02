9ea08ff3eaa0f565440298063c20c1b28d1e0629
Shawn Almeida <55237616+shawnyplayz@users.noreply.github.com>
2021-04-02 16:51:39 +0530

    created a react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
+//import './Header';
+import { Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+//import Header from './Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/checkout">checkout page</Link>
+            </li>
+            <li>
+            <Link to="/login">Login page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+        <Route path ="/checkout">
+          <h1>Hi this is the checkout page</h1>
+        </Route>
+        <Route path="/login">
+          <h1>This is the login page</h1>
+        </Route>
+        <Route path='/'>
+          <h1>This is the main page</h1>
+        </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="Header">
+         <p>This is the header file</p>   
+        </div>
+    )
+}
+
+export default Header
